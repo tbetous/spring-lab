aa93de9a188179b214221186b9ebef7fc1dcde88
tbetous <tbetous@gmail.com>
2015-07-31 23:28:38 +0200

    Change structure for helloworld projects

--- spring-lab-helloworld/src/main/java/com/tbetous/springlab/app/App.java
+++ spring-lab-helloworld/src/main/java/com/tbetous/springlab/app/App.java
@@ -1,14 +1,16 @@
-package com.tbetous.springlab.helloworld;
+package com.tbetous.springlab.app;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.tbetous.springlab.helloworld.HelloWorld;
 
 public class App {
 
     public static void main(String[] args) {
         
         // Create the spring context
-        ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring-module.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/spring-module.xml");
         
         // Get my HelloWorld bean from the Spring context
         HelloWorld obj = (HelloWorld) context.getBean("helloBean");
