33068f8e20ef434dbf3b633dcc5c7eb68b9f82bc
tbetous <tbetous@gmail.com>
2015-08-30 22:45:19 +0200

    Fix pom.xml and add controller and view

---
+++ spring-lab-mvc-orm-example/src/main/java/com/tbetous/springlab/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.tbetous.springlab.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.tbetous.springlab.dao.UserDao;
+import com.tbetous.springlab.data.User;
+import com.tbetous.springlab.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+    
+    @Autowired
+    @Qualifier("userDaoJDBC")
+    public UserDao userDao;
+
+    @Override
+    public List<User> getUsers() {
+        return userDao.getUsers();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    @Override
+    public Optional<User> getUsers(Long id) {
+        return userDao.getUsers(id);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userDao.deleteUser(id);
+    }
+    
+}
